6a28ccd9635e9333e1075163d644137ce1ee23c5
Michael Johnson <mj66@hawaii.edu>
2019-11-19 11:33:28 -1000

    Updated Landing 1/2

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Container,Card,Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='foodmoodbg-landing'>
+          <Container>
+            <h1 className='landing'>WELCOME TO UHM FOOD MOOD</h1>
+            <h3 className='landing'>Your favorites. All in one place!</h3>
+            <h4 className= 'landing'>UHM Food Mood strives to give an easily accesible catalog of all food options at UH Manoa.
+              Users can pick their favorites on campus, from food trucks to options at Campus Center and Paradise Palms.
+              They will be able to see which of their favorites are available right at this moment.
+              Vendors can add their own menu items to the catalog of UH food options.
+            </h4>
+            <Card.Group itemsPerRow={2}>
+              <Card>
+                  <Card.Content>
+                    <Image centered size= 'medium' src='https://static.vecteezy.com/system/resources/previews/000/134/503/non_2x/free-vector-food-illustration.jpg' />
+                    <Card.Header>Today's Top Picks</Card.Header>
+                  </Card.Content>
+              </Card>
+              <Card>
+                <Card.Content>
+                  <Image centered size= 'medium' src='https://static.vecteezy.com/system/resources/previews/000/134/503/non_2x/free-vector-food-illustration.jpg' />
+                  <Card.Header>Today's Top Picks</Card.Header>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+          </Container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        </div>
     );
   }
 }
