5142f5ed0584143dd211a84bf8ddff429321fe8a
kellikt <44486938+kellikt@users.noreply.github.com>
2019-11-19 10:39:15 -1000

    Revert "Added MenuItems Collections and Admin/Vendor Pages"
    This reverts commit 7d6426055bbe02cc89a250173645ce6f78555fa4.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-import { MenuItems } from '../../api/menu/MenuItems';
 
 /* eslint-disable no-console */
 
@@ -15,18 +14,5 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
-  }
-}
-
-function addMenu(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  MenuItems.insert(data);
-}
-
-/** Initialize the collection if empty. */
-if (MenuItems.find().count() === 0) {
-  if (Meteor.settings.defaultMenu) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultMenu.map(data => addMenu(data));
   }
 }
