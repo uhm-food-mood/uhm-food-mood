6921720c258d734a8158a5e6ce684d3724f5bc12
kellikt <44486938+kellikt@users.noreply.github.com>
2019-11-19 10:39:24 -1000

    Revert "Revert "Added MenuItems Collections and Admin/Vendor Pages""
    This reverts commit 5142f5ed0584143dd211a84bf8ddff429321fe8a.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,25 +13,32 @@ class NavBar extends React.Component {
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>meteor-application-template</Header>
+          <Header inverted as='h1' className='josefin'>UHM FOOD MOOD</Header>
         </Menu.Item>
+        <Menu.Item as={NavLink} activeClassName="active"
+                   exact to="/" key='add' className='montserrat'>FOOD LISTINGS</Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+            [<Menu.Item as={NavLink} className='montserrat' activeClassName="active"
+                        exact to="/list" key='add'>YOUR LISTINGS</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+            <Menu.Item as={NavLink} className='montserrat' activeClassName="active"
+                       exact to="/admin" key='admin'>MANAGE LISTINGS</Menu.Item>
+        ) : ''}
+        {Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
+            <Menu.Item as={NavLink} className='montserrat' activeClassName="active"
+                       exact to="/vendor" key='vendor'>VENDOR LISTINGS</Menu.Item>
         ) : ''}
         <Menu.Item position="right">
           {this.props.currentUser === '' ? (
-            <Dropdown text="Login" pointing="top right" icon={'user'}>
+            <Dropdown className='josefin' text="LOGIN" pointing="top right" icon={'user'}>
               <Dropdown.Menu>
                 <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
                 <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
               </Dropdown.Menu>
             </Dropdown>
           ) : (
-            <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
+            <Dropdown className='montserrat' text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
                 <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
