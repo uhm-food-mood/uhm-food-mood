768f9b2ba4af2bd3266acb9478de92318647cda4
Michael Johnson <mj66@hawaii.edu>
2019-11-19 17:35:24 -1000

    Updated Landing Image Layout

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card } from 'semantic-ui-react';
+import { Container, Card, Grid, Image } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -13,13 +13,22 @@ class Landing extends React.Component {
         <div className='foodmoodbg'>
           <Container>
             <h1 className='landing'>WELCOME TO UHM FOOD MOOD</h1>
-            <h3 className='landing'>Your favorites. All in one place!</h3>
-            <h4 className= 'landing'>UHM Food Mood strives to give an easily
+            <Grid>
+              <Grid.Column width={8}>
+                < Image src='images/landing-1.png'/>
+              </Grid.Column>
+              <Grid.Column width={8} >
+                <div className='landing-description'>
+            <h1 className='landing'>Your favorites. All in one place!</h1>
+            <h2 className= 'landing'>UHM Food Mood strives to give an easily
               accessible catalog of all food options at UH Manoa.
               Users can pick their favorites on campus, from food trucks to options at Campus Center and Paradise Palms.
               They will be able to see which of their favorites are available right at this moment.
               Vendors can add their own menu items to the catalog of UH food options.
-            </h4>
+            </h2>
+                </div>
+              </Grid.Column>
+            </Grid>
             <h1 className='landing'>Students&#39; Top Choices</h1>
             <Card.Group itemsPerRow={3}>
               {this.props.menuitems.map((menuitems, index) => <MenuItem key={index} menuitems={menuitems} />)}
