3d85270e69c48e955effd3286e05babe9c09d301
kellikt <44486938+kellikt@users.noreply.github.com>
2019-11-19 15:30:00 -1000

    Fixed ESLint Errors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -9,7 +9,8 @@ class Landing extends React.Component {
           <Container>
             <h1 className='landing'>WELCOME TO UHM FOOD MOOD</h1>
             <h3 className='landing'>Your favorites. All in one place!</h3>
-            <h4 className= 'landing'>UHM Food Mood strives to give an easily accesible catalog of all food options at UH Manoa.
+            <h4 className= 'landing'>UHM Food Mood strives to give an easily
+              accesible catalog of all food options at UH Manoa.
               Users can pick their favorites on campus, from food trucks to options at Campus Center and Paradise Palms.
               They will be able to see which of their favorites are available right at this moment.
               Vendors can add their own menu items to the catalog of UH food options.
@@ -20,13 +21,15 @@ class Landing extends React.Component {
                     <Image centered size= 'medium'
                            /* eslint-disable-next-line max-len */
                            src='https://static.vecteezy.com/system/resources/previews/000/134/503/non_2x/free-vector-food-illustration.jpg' />
-                    <Card.Header>Today's Top Picks</Card.Header>
+                    <Card.Header>Today&#39;s Top Picks</Card.Header>
                   </Card.Content>
               </Card>
               <Card>
                 <Card.Content>
-                  <Image centered size= 'medium' src='https://static.vecteezy.com/system/resources/previews/000/134/503/non_2x/free-vector-food-illustration.jpg' />
-                  <Card.Header>Today's Top Picks</Card.Header>
+                  <Image centered size= 'medium'
+                         {/* eslint-disable-next-line max-len */}
+                         src='https://static.vecteezy.com/system/resources/previews/000/134/503/non_2x/free-vector-food-illustration.jpg' />
+                  <Card.Header>Today&#39;s Top Picks</Card.Header>
                 </Card.Content>
               </Card>
             </Card.Group>
