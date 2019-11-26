10a7efed36056b26ad2fa86d017dc4f6643a9537
Michael Johnson <mj66@hawaii.edu>
2019-11-26 11:39:03 -1000

    Cleaned Up Default Files

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,25 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
 import { MenuItems } from '../../api/menu/MenuItems';
 import { Reviews } from '../../api/review/Reviews';
 
 /** This subscription publishes only the documents associated with the logged in user */
-Meteor.publish('Stuff', function publish() {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.find({ owner: username });
-  }
-  return this.ready();
-});
 
-/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('StuffAdmin', function publish() {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.find();
-  }
-  return this.ready();
-});
 
 Meteor.publish('MenuItems', function publish() {
   if (this.userId) {
