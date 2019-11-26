1d97a90e36e68c738ccdf60ffff309f330c702af
kellikt <44486938+kellikt@users.noreply.github.com>
2019-11-26 12:52:11 -1000

    Testing changes to MenuItem
    Ideally will want to implement a feature so when the user clicks on the heart button, it will create a copy of the MenuItem with them as the owner (but master but be "no")

--- app/imports/ui/components/MenuItem.jsx
+++ app/imports/ui/components/MenuItem.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 import { Card, Image, Label, Icon, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import { MenuItems } from '../../api/menu/MenuItems';
+import swal from 'sweetalert';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class MenuItem extends React.Component {
+
+  submit(data) {
+    MenuItems.insert({
+          name,
+          image,
+          vendor,
+          price,
+          availability,
+          starting,
+          startingPeriod,
+          ending,
+          endingPeriod,
+          vegan,
+          ethnicity,
+          owner,
+          master,
+        },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Item added successfully', 'success');
+            formRef.reset();
+          }
+        });
+  }
 
   render() {
     return (
@@ -19,7 +47,7 @@ class MenuItem extends React.Component {
             <Card.Description>{this.props.menuitems.starting}:00 {this.props.menuitems.startingPeriod} -
               {this.props.menuitems.ending}:00 {this.props.menuitems.endingPeriod}
             </Card.Description>
-            <Button icon>
+            <Button icon onClick={() => this.removeItem(this.props.menuitems._id)}>
               <Icon name='heart' />
             </Button>
             <Label color='red'>{this.props.menuitems.ethnicity}</Label>
