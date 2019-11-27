d5d436229712e20d4500851858a7a61e8b337520
kellikt <44486938+kellikt@users.noreply.github.com>
2019-11-26 15:06:43 -1000

    Update MenuItem.jsx

--- app/imports/ui/components/MenuItem.jsx
+++ app/imports/ui/components/MenuItem.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 import { Card, Image, Label, Icon, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import moment from 'moment';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+
+function available(starting, startingPeriod, ending, endingPeriod) {
+  let start = moment().hour(parseInt(starting));
+  if (startingPeriod === 'PM') {
+    start = moment().hour(parseInt(starting) + 12);
+  }
+  console.log(start);
+  let end = moment().hour(parseInt(ending));
+  if (endingPeriod === 'PM') {
+    end = moment().hour(parseInt(ending) + 12);
+  }
+  console.log(end);
+  const now = moment().hour();
+  console.log(now);
+  if (now.getHours > start.getHours && now.getHours < end.getHours) {
+    return true;
+  }
+  return false;
+}
+
 class MenuItem extends React.Component {
 
   render() {
@@ -19,6 +40,14 @@ class MenuItem extends React.Component {
             <Card.Description>{this.props.menuitems.starting}:00 {this.props.menuitems.startingPeriod} -
               {this.props.menuitems.ending}:00 {this.props.menuitems.endingPeriod}
             </Card.Description>
+            {available(this.props.menuitems.starting,
+                this.props.menuitems.startingPeriod,
+                this.props.menuitems.ending,
+                this.props.menuitems.endingPeriod) ? (
+            <Card.Description>
+              Available now!
+            </Card.Description>
+            ) : ''}
             <Button icon>
               <Icon name='heart' />
             </Button>
