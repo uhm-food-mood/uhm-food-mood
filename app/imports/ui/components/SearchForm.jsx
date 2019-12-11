27e4b63081dd5a86063fdbb3d6de0096c9485ae1
kellikt <44486938+kellikt@users.noreply.github.com>
2019-12-10 19:09:47 -1000

    Added button to sort menu items by rating

--- app/imports/ui/components/SearchForm.jsx
+++ app/imports/ui/components/SearchForm.jsx
@@ -6,8 +6,10 @@ import { withTracker } from 'meteor/react-meteor-data';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { MenuItems } from '../../api/menu/MenuItems';
+import { Reviews } from '../../api/review/Reviews';
 
 let active = false;
+let sorted = false;
 
 class SearchForm extends React.Component {
 
@@ -93,6 +95,19 @@ class SearchForm extends React.Component {
     return active;
   }
 
+  isSorted = () => {
+    if (sorted === true) {
+      sorted = false;
+      this.forceUpdate();
+      return sorted;
+    }
+    if (sorted === false) {
+      sorted = true;
+    }
+    this.forceUpdate();
+    return sorted;
+  }
+
   renderPage() {
     return (
         <div>
@@ -110,17 +125,33 @@ class SearchForm extends React.Component {
                 Food Available Now
               </Button>
           ) : '' }
+          {!sorted ? (
+              <Button onClick={this.isSorted}>
+                Sort by rating
+              </Button>
+          ) : '' }
+          {sorted ? (
+              <Button color='green' onClick={this.isSorted}>
+                Sort by rating
+              </Button>
+          ) : '' }
           <br/>
           <br/>
-          {active === true ? (
+          {active === true && sorted === false ? (
               <Card.Group itemsPerRow={3}>
                 {this.props.availableitems.filter(this.searchItems).map((menuitems, index) => <MenuItem
                     key={index} menuitems={menuitems} />)}
               </Card.Group>
           ) : ''}
-          {active === false ? (
+          {active === false && sorted === false ? (
               <Card.Group itemsPerRow={3}>
                 {this.props.menuitems.filter(this.searchItems).map((menuitems, index) => <MenuItem
+                    key={index} menuitems={menuitems} />)}
+              </Card.Group>
+          ) : ''}
+          {sorted === true ? (
+              <Card.Group itemsPerRow={3}>
+                {this.props.sorteditems.filter(this.searchItems).map((menuitems, index) => <MenuItem
                     key={index} menuitems={menuitems} />)}
               </Card.Group>
           ) : ''}
@@ -132,6 +163,7 @@ class SearchForm extends React.Component {
 SearchForm.propTypes = {
   menuitems: PropTypes.array.isRequired,
   availableitems: PropTypes.array.isRequired,
+  sorteditems: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -165,10 +197,38 @@ export default withTracker(() => {
   }
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('AllMenuItems');
+  const subscription2 = Meteor.subscribe('Reviews');
   const menu = MenuItems.find({}).fetch();
   return {
     menuitems: MenuItems.find({}).fetch(),
     availableitems: menu.filter(available),
-    ready: subscription.ready(),
+    sorteditems: menu.sort(function (a, b) {
+      const first = a;
+      const total1 = Reviews.find({ menuId: first._id }).fetch();
+      let average1 = Object.values(total1).reduce((t, { rating }) => t + rating, 0);
+      // console.log(average);
+      const length = Reviews.find({ menuId: first._id }).fetch().length;
+      // console.log(length);
+      average1 /= length;
+      // eslint-disable-next-line no-restricted-globals
+      if (isNaN(average1)) {
+        average1 = 0;
+      }
+      // console.log(`average1: ${average1}`);
+      const second = b;
+      const total2 = Reviews.find({ menuId: second._id }).fetch();
+      let average2 = Object.values(total2).reduce((t, { rating }) => t + rating, 0);
+      // console.log(average);
+      const length2 = Reviews.find({ menuId: second._id }).fetch().length;
+      // console.log(length);
+      average2 /= length2;
+      // eslint-disable-next-line no-restricted-globals
+      if (isNaN(average2)) {
+        average2 = 0;
+      }
+      // console.log(`average2: ${average2}`);
+      return average2 - average1;
+    }),
+    ready: subscription.ready() && subscription2.ready(),
   };
 })(SearchForm);
